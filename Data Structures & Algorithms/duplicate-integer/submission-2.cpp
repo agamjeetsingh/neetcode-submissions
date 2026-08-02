018dd06a3de49ee431c7472ec0c0ab7c4c6c0abf
Agamjeet Singh <135738729+agamjeetsingh@users.noreply.github.com>
2026-08-02 23:50:04 +0100

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-2.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        std::unordered_set<int> s;
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (s.contains(i)) {
+                return true;
+            }
+            s.add(nums[i]);
+        }
+        return false;
+    }
+};
