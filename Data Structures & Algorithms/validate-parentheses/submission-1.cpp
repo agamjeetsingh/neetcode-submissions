63553c652cd039b16668ee061badff6eb3361dee
Agamjeet Singh <135738729+agamjeetsingh@users.noreply.github.com>
2026-08-05 18:42:52 +0100

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+
+        for (char c : s) {
+            if (c == '(' || c == '[' || c == '{') {
+                st.push(c);
+            }
+            if (c == ')') {
+                if (st.empty() || st.top() != '(') {
+                    return false;
+                } else {
+                    st.pop();
+                }
+            }
+            if (c == ']') {
+                if (st.empty() || st.top() != '[') {
+                    return false;
+                } else {
+                    st.pop();
+                }
+            }
+            if (c == '}') {
+                if (st.empty() || st.top() != '{') {
+                    return false;
+                } else {
+                    st.pop();
+                }
+            }
+        }
+        return st.empty();
+    }
+};
