f3c09f4a0a20f8f29008468bf5ec062cf2ff7653
Agamjeet Singh <135738729+agamjeetsingh@users.noreply.github.com>
2026-08-10 20:25:17 +0100

    Add: eating-bananas - submission-1

---
+++ Data Structures & Algorithms/eating-bananas/submission-1.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int lo = 1;
+        int hi = *max_element(piles.begin(), piles.end());
+
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            
+            if (works(piles, h, mid)) {
+                hi = mid - 1;
+            } else {
+                lo = mid + 1;
+            }
+        }
+        return lo;
+    }
+
+    bool works(vector<int>& piles, int h, int k) {
+        int cost = 0;
+        for (int pile : piles) {
+            cost += (pile + k - 1) / k;
+        }
+        return cost <= h;
+    }
+};
