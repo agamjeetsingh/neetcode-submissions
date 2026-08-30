2da3aa2f55a35241c968cc78e2630d02a72e40e0
Agamjeet Singh <135738729+agamjeetsingh@users.noreply.github.com>
2026-08-31 00:18:35 +0100

    Add: hand-of-straights - submission-2

---
+++ Data Structures & Algorithms/hand-of-straights/submission-2.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool isNStraightHand(vector<int>& hand, int groupSize) {
+        if (hand.size() % groupSize != 0) return false;
+        map<int, int> freqs;
+        set<int> s;
+        for (int num: hand) {
+            freqs[num]++;
+            s.insert(num);
+        }
+        while (!s.empty()) {
+            int count = 0;
+            int last;
+            bool first = true;
+            vector<int> removal;
+            for (int num: s) {
+                if (++count > groupSize) break;
+                if (!first && num != last + 1) return false;
+                first = false;
+                last = num;
+                if (--freqs[num] == 0) removal.push_back(num);
+            }
+            if (count < groupSize) return false;
+            for (int num: removal) {
+                s.erase(num);
+            }
+        }
+
+        return true;
+    }
+};
