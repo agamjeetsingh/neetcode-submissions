288cbe6ec43cf7afdea238937b2b2c590103f81b
Agamjeet Singh <135738729+agamjeetsingh@users.noreply.github.com>
2026-08-31 02:44:37 +0100

    Add: meeting-schedule-ii - submission-3

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-3.cpp
@@ -0,0 +1,40 @@
+/**
+ * Definition of Interval:
+ * class Interval {
+ * public:
+ *     int start, end;
+ *     Interval(int start, int end) {
+ *         this->start = start;
+ *         this->end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+public:
+    int minMeetingRooms(vector<Interval>& intervals) {
+        sort(intervals.begin(), intervals.end(), [](const Interval& i1, const Interval& i2) {
+            if (i1.start == i2.start) return i2.end > i1.end;
+            return i1.start < i2.start;
+        });
+
+        multiset<int> endTimes;
+
+        for (auto& interval: intervals) {
+            // cout << interval.start << " " << interval.end << endl;
+            if (endTimes.empty()) {
+                endTimes.insert(interval.end);
+            } else {
+                int earliestEndTime = *endTimes.begin();
+                if (earliestEndTime > interval.start) {
+                    endTimes.insert(interval.end);
+                } else {
+                    endTimes.erase(endTimes.begin());
+                    endTimes.insert(interval.end);
+                }
+            }
+        }
+
+        return endTimes.size();
+    }
+};
